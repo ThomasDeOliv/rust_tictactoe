6325fadeb81b6ad15c14de31638224ec17d92b0d
eclipse902 <68570590+ThomasDeOliv@users.noreply.github.com>
2024-12-24 06:21:25 +0100

    Create program

--- src/models/player/allowed_char/mod.rs
+++ src/models/player/allowed_char/mod.rs
@@ -1,5 +1,13 @@
-#[derive(PartialEq)]
 pub enum AllowedChar {
     X,
     O,
+}
+
+impl PartialEq for AllowedChar {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (AllowedChar::X, AllowedChar::X) | (AllowedChar::O, AllowedChar::O) => true,
+            _ => false,
+        }
+    }
 }
